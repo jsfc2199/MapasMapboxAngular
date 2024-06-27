0f7ca08ea132f8a39cd67b225f91a95f32ae3a9c
Juanse Franco <jsfc2199@gmail.com>
2024-06-27 14:27:14 -0500

    diseño de zoom range page

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import {Map} from 'mapbox-gl'
 @Component({
   selector: 'app-zoom-range-page',
   templateUrl: './zoom-range-page.component.html',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class ZoomRangePageComponent {
 
+  @ViewChild('map') divMap?: ElementRef;
+
+  //lo usamos aquí para esperar que todos los componentes hijos carguen
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw Error('Map elemento required');
+
+    //esta implementación funciona pero es herrada en función de lo que se quiere hacer
+    const map = new Map({
+      accessToken:
+        'pk.eyJ1IjoianNmYzIxOTkiLCJhIjoiY2x4eGtnczd4MmVuazJpcHRzdXdqcmh3ZSJ9.1ojpSPj3K7ubSwUr7rgeAw',
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 }
