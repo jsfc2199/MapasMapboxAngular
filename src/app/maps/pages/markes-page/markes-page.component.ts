967c9af92693706918ce0cf877e69f74f4de908a
Juanse Franco <jsfc2199@gmail.com>
2024-06-27 15:11:52 -0500

    inicio de marcadores

--- src/app/maps/pages/markes-page/markes-page.component.ts
+++ src/app/maps/pages/markes-page/markes-page.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'app-markes-page',
   templateUrl: './markes-page.component.html',
-  styleUrl: './markes-page.component.css'
+  styleUrl: './markes-page.component.css',
 })
 export class MarkesPageComponent {
+  @ViewChild('map') divMap?: ElementRef;
 
+  map?: Map;
+  currentCenter: LngLat = new LngLat(-74.5, 40);
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw Error('Map elemento required');
+    this.map = new Map({
+      accessToken:
+        'pk.eyJ1IjoianNmYzIxOTkiLCJhIjoiY2x4eGtnczd4MmVuazJpcHRzdXdqcmh3ZSJ9.1ojpSPj3K7ubSwUr7rgeAw',
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentCenter, // starting position [lng, lat]
+      zoom: 10, // starting zoom
+    });
+
+    //referencia de como crear un marcador
+    // const markerHtml = document.createElement('div')
+    // markerHtml.innerHTML = 'Juan'
+
+    // const marker= new Marker({
+    //   element: markerHtml
+    // })
+    // .setLngLat(this.currentCenter)
+    // .addTo(this.map!)
+  }
 }
