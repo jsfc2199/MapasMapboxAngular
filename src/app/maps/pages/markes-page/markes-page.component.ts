6998f4eb5c2465defd4adc255c2a4b8d2c20cacd
Juanse Franco <jsfc2199@gmail.com>
2024-06-27 15:44:46 -0500

    añadiendo y leyendo marcadores con el localStorage

--- src/app/maps/pages/markes-page/markes-page.component.ts
+++ src/app/maps/pages/markes-page/markes-page.component.ts
@@ -1,9 +1,14 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { LngLat, Map, Marker } from 'mapbox-gl';
 
-interface MarkerColor{
-  color: string,
-  marker: Marker
+interface MarkerColor {
+  color: string;
+  marker: Marker;
+}
+
+interface PlainMarker {
+  color: string;
+  lngLat: number[];
 }
 @Component({
   selector: 'app-markes-page',
@@ -15,7 +20,7 @@ export class MarkesPageComponent {
 
   map?: Map;
   currentCenter: LngLat = new LngLat(-74.5, 40);
-  currentMarkers: MarkerColor[] = []
+  currentMarkers: MarkerColor[] = [];
 
   ngAfterViewInit(): void {
     if (!this.divMap) throw Error('Map elemento required');
@@ -28,6 +33,8 @@ export class MarkesPageComponent {
       zoom: 10, // starting zoom
     });
 
+    this.readFromLocalStorage()
+
     //referencia de como crear un marcador
     // const markerHtml = document.createElement('div')
     // markerHtml.innerHTML = 'Juan'
@@ -39,35 +46,67 @@ export class MarkesPageComponent {
     // .addTo(this.map!)
   }
 
-  createMarker(){
-    if(!this.map) return
-    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16)); //hexagesimal de manra aleatoria
-    const lngLat = this.map.getCenter()
-    this.addMarker(lngLat, color)
+  createMarker() {
+    if (!this.map) return;
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    ); //hexagesimal de manra aleatoria
+    const lngLat = this.map.getCenter();
+    this.addMarker(lngLat, color);
   }
 
-  addMarker(lngLat: LngLat, color: string){
-    if(!this.map) return
+  addMarker(lngLat: LngLat, color: string) {
+    if (!this.map) return;
 
     const marker = new Marker({
       color: color,
       draggable: true, //propiedad que permite mover el marcador
-    }).setLngLat(lngLat).addTo(this.map)
+    })
+      .setLngLat(lngLat)
+      .addTo(this.map);
 
     this.currentMarkers.push({
-      color, marker
-    })
+      color,
+      marker,
+    });
+
+    this.saveToLocalStorage();
   }
 
-  deleteMarker(i: number){
-    this.currentMarkers[i].marker.remove()
-    this.currentMarkers.splice(i, 1)
+  deleteMarker(i: number) {
+    this.currentMarkers[i].marker.remove();
+    this.currentMarkers.splice(i, 1);
   }
 
-  flyTo(marker: Marker){
+  flyTo(marker: Marker) {
     this.map?.flyTo({
       zoom: 14,
-      center: marker.getLngLat()
+      center: marker.getLngLat(),
+    });
+  }
+
+  saveToLocalStorage() {
+    const plainMarkers: PlainMarker[] = this.currentMarkers.map(
+      ({ color, marker }) => {
+        return {
+          color,
+          lngLat: marker.getLngLat().toArray(),
+        };
+      }
+    );
+
+    localStorage.setItem('plainMarkers', JSON.stringify(plainMarkers));
+  }
+
+  readFromLocalStorage() {
+    const plainMarkersString = localStorage.getItem('plainMarkers') ?? '[]'
+    const plainMarkers: PlainMarker[] = JSON.parse(plainMarkersString)
+
+    plainMarkers.forEach(({color, lngLat}) => {
+      const [lng, lat] = lngLat
+      const coords = new LngLat(lng,lat)
+
+      this.addMarker(coords, color)
     })
   }
 }
