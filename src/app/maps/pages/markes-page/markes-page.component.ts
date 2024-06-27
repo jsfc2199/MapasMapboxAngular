f1ac3143fe1629a672420509a10ffa360ea90122
Juanse Franco <jsfc2199@gmail.com>
2024-06-27 13:08:32 -0500

    Creacion de componentes y rutas

---
+++ src/app/maps/pages/markes-page/markes-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-markes-page',
+  templateUrl: './markes-page.component.html',
+  styleUrl: './markes-page.component.css'
+})
+export class MarkesPageComponent {
+
+}
