1e80af326e9f682b0c64847446d7a4209c78b29b
Juanse Franco <jsfc2199@gmail.com>
2024-06-27 13:15:36 -0500

    Side menu template

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface MenuItem{
+  name: string;
+  route: string;
+}
 @Component({
-  selector: 'app-side-menu',
+  selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
 
+  menuItems: MenuItem[]=[
+    {route: '/maps/fullscreen', name: 'Full-Screen'},
+    {route: '/maps/zoom-range', name: 'Zoom-Range'},
+    {route: '/maps/markers', name: 'Markers'},
+    {route: '/maps/properties', name: 'Houses'},
+
+  ]
 }
