c1d474e9bc18181143f7149e77b4dc6c48add402
Juanse Franco <jsfc2199@gmail.com>
2024-06-27 14:44:56 -0500

    Controlar el valor del zoom con la barra

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -9,18 +9,48 @@ export class ZoomRangePageComponent {
 
   @ViewChild('map') divMap?: ElementRef;
 
+  zoom = 10
+  map?: Map;
+
   //lo usamos aquí para esperar que todos los componentes hijos carguen
   ngAfterViewInit(): void {
     if (!this.divMap) throw Error('Map elemento required');
-
-    //esta implementación funciona pero es herrada en función de lo que se quiere hacer
-    const map = new Map({
+    this.map = new Map({
       accessToken:
         'pk.eyJ1IjoianNmYzIxOTkiLCJhIjoiY2x4eGtnczd4MmVuazJpcHRzdXdqcmh3ZSJ9.1ojpSPj3K7ubSwUr7rgeAw',
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
     });
+
+    this.mapListeners()
+  }
+
+  mapListeners(){
+    if(!this.map)  throw Error('Map elemento required');
+
+    this.map.on('zoom', (z) => {
+      this.zoom = this.map!.getZoom()
+    })
+
+    this.map.on('zoomend', (z) => {
+      if(this.map!.getZoom()< 18) return
+      this.map!.zoomTo(18)
+    })
+
+  }
+
+  zoomIn(){
+    this.map?.zoomIn()
+  }
+  zoomOut(){
+    this.map?.zoomOut()
+
+  }
+
+  zoomChanged(value: string){
+    this.zoom = +value;
+    this.map!.zoomTo(this.zoom)
   }
 }
